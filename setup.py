b224b91e012b0be03fec57e332a49f7840b5a354
Samapriya Roy <samapriya.roy@gmail.com>
2022-05-27 14:37:19 -0500

    updated v0.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setuptools.setup(
     name="cogee",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     url="https://github.com/samapriya/cogee",
     install_requires=[
